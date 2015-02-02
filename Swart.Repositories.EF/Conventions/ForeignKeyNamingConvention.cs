9c32e11dafa9e75fdef8e24636d1e2e8266a73c3
moahelmy <muhammad.cs@gmail.com>
2015-02-03 05:22:49 +1100

    Apply resharper suggests

--- Swart.Repositories.EF/Conventions/ForeignKeyNamingConvention.cs
+++ Swart.Repositories.EF/Conventions/ForeignKeyNamingConvention.cs
@@ -2,7 +2,7 @@
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
-namespace Infrastructure.Repositories.EntityFrameowrk.Conventions
+namespace Swart.Repositories.EntityFramework.Conventions
 {
     /// <summary>
     /// Provides a convention for fixing the independent association (IA) foreign key column names.
@@ -13,18 +13,16 @@ namespace Infrastructure.Repositories.EntityFrameowrk.Conventions
         public void Apply(AssociationType association, DbModel model)
         {
             // Identify a ForeignKey properties (including IAs)
-            if (association.IsForeignKey)
+            if (!association.IsForeignKey) return;
+            // rename FK columns
+            var constraint = association.Constraint;
+            if (DoPropertiesHaveDefaultNames(constraint.FromProperties, constraint.ToRole.Name, constraint.ToProperties))
             {
-                // rename FK columns
-                var constraint = association.Constraint;
-                if (DoPropertiesHaveDefaultNames(constraint.FromProperties, constraint.ToRole.Name, constraint.ToProperties))
-                {
-                    NormalizeForeignKeyProperties(constraint.FromProperties);
-                }
-                if (DoPropertiesHaveDefaultNames(constraint.ToProperties, constraint.FromRole.Name, constraint.FromProperties))
-                {
-                    NormalizeForeignKeyProperties(constraint.ToProperties);
-                }
+                NormalizeForeignKeyProperties(constraint.FromProperties);
+            }
+            if (DoPropertiesHaveDefaultNames(constraint.ToProperties, constraint.FromRole.Name, constraint.FromProperties))
+            {
+                NormalizeForeignKeyProperties(constraint.ToProperties);
             }
         }
 
@@ -47,16 +45,16 @@ namespace Infrastructure.Repositories.EntityFrameowrk.Conventions
 
         private void NormalizeForeignKeyProperties(ReadOnlyMetadataCollection<EdmProperty> properties)
         {
-            for (int i = 0; i < properties.Count; ++i)
+            for (var i = 0; i < properties.Count; ++i)
             {
-                string defaultPropertyName = properties[i].Name;
-                int ichUnderscore = defaultPropertyName.IndexOf('_');
+                var defaultPropertyName = properties[i].Name;
+                var ichUnderscore = defaultPropertyName.IndexOf('_');
                 if (ichUnderscore <= 0)
                 {
                     continue;
                 }
-                string navigationPropertyName = defaultPropertyName.Substring(0, ichUnderscore);
-                string targetKey = defaultPropertyName.Substring(ichUnderscore + 1);
+                var navigationPropertyName = defaultPropertyName.Substring(0, ichUnderscore);
+                var targetKey = defaultPropertyName.Substring(ichUnderscore + 1);
 
                 string newPropertyName;
                 if (targetKey.StartsWith(navigationPropertyName))
